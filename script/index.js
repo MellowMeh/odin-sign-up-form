960b2cc0aa6ee6a1d29a99468f360e0cf8bfba83
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-07-02 17:39:10 -0400

    Add list element query selectors

---
+++ script/index.js
@@ -0,0 +1,6 @@
+let firstNameBox = document.querySelector('.first-name-box');
+let lastNameBox = document.querySelector('.last-name-box');
+let emailBox = document.querySelector('.email-box');
+let phoneNumberBox = document.querySelector('.phone-number-box');
+let passwordBox = document.querySelector('.password-box');
+let confirmPasswordBox = document.querySelector('.confirm-password-box');
