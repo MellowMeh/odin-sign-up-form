7f94bcef7c24f22ed079bc48d098a763f755c901
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-07-02 19:10:50 -0400

    Add remove form text functions

--- script/index.js
+++ script/index.js
@@ -10,3 +10,27 @@ let passwordBox = document.querySelector('.password-box');
 let passwordText = document.querySelector('.password-box span');
 let confirmPasswordBox = document.querySelector('.confirm-password-box');
 let confirmPasswordText = document.querySelector('.confirm-password-box span');
+
+firstNameText.addEventListener('click', () => {
+    firstNameBox.removeChild(firstNameText);
+})
+
+lastNameText.addEventListener('click', () => {
+    lastNameBox.removeChild(lastNameText);
+})
+
+emailText.addEventListener('click', () => {
+    emailBox.removeChild(emailText);
+})
+
+phoneNumberText.addEventListener('click', () => {
+    phoneNumberBox.removeChild(phoneNumberText);
+})
+
+passwordText.addEventListener('click', () => {
+    passwordBox.removeChild(passwordText);
+})
+
+confirmPasswordText.addEventListener('click', () => {
+    confirmPasswordBox.removeChild(confirmPasswordText);
+})
